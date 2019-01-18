5a9c7eeb3d990f1908648447daf8b2e2d87d9c64
d-derks <2debbiederks@gmail.com>
2019-01-18 11:34:53 +0100

    [FEATURE] Components: Add several components HTML5/CCS3 - Pageheader - Brand - Mainnav

---
+++ src/components/Pageheader/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Brand from '../Brand';
+import Mainnav from '../Mainnav';
+import './pageheader.scss';
+
+export default function Pageheader() {
+    return(
+        <header className='b-pageheader'>
+            <div className="pageheader-inner">
+                <div className="pageheader-brand">
+                    <Brand />
+                </div>
+                <div className="pageheader-nav">
+                    <Mainnav />
+                </div>
+            </div>
+        </header>
+    )
+}
