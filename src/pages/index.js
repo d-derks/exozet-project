470ade6c49decc470d944cdf4a94f9dbd3dbf364
d-derks <2debbiederks@gmail.com>
2019-01-18 11:09:09 +0100

    [GENERAL] General: small changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Pageheader from '../components/Pageheader';
 import './page.scss';
 
 export default () => (
     <div className='l-page'>
         <div className="page-inner">
-            <Pageheader/>
         </div>
     </div>
 )
