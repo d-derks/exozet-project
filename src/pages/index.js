a1b46bec73b0015d8367cd029a7b3ab4d886959b
d-derks <2debbiederks@gmail.com>
2019-01-19 08:00:23 +0100

    [FEATURE] Layout: create structure page with styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,23 @@ import './page.scss';
 import Togglebtn from '../components/Togglebtn';
 
 export default () => (
-    <div className='l-page'>
-        <div className="page-inner">
+    <React.Fragment>
+        <div className='l-page'>
+        <div className="page-wrapper">
             <Pageheader/>
             <Togglebtn/>
+            <div className="page-inner">
+                <main className="page-main">
+                    <div className="page-main-inner">
+                    </div>
+                </main>
+                <aside className="page-sideinfo">
+                </aside>
+            </div>
         </div>
-    </div>
+        <footer className="page-footer">
+        </footer>
+        </div>
+    </React.Fragment>
 )
 //
