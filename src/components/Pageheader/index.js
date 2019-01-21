dc1f369e622b048950fa08e96f8e41693941e7d7
d-derks <2debbiederks@gmail.com>
2019-01-21 11:17:07 +0100

    [FEATURE] Pageheader/Mainnav: Deleted wrappers pageheader and brand. Mainnav is visible again screensize small

--- src/components/Pageheader/index.js
+++ src/components/Pageheader/index.js
@@ -7,12 +7,8 @@ export default function Pageheader() {
     return(
         <header className="b-pageheader">
             <div className="pageheader-inner">
-                <div className="pageheader-brand">
-                    <Brand />
-                </div>
-                <div className="pageheader-nav">
-                    <Mainnav />
-                </div>
+                <Brand />
+                <Mainnav />
             </div>
         </header>
     )
