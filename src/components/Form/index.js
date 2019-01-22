8afeffc1c09a1b29cebbc68597f23ae99686d66b
d-derks <2debbiederks@gmail.com>
2019-01-22 12:08:46 +0100

    [FEATURE] Form: Adjustments Form component - adjusted structure - Add aria tag error message - Add validation input

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -3,61 +3,115 @@ import BtnDefault from '../Buttons';
 import './form.scss';
 
 export default function Form() {
+
     return(
         <form action="" className="b-form">
-            <fieldset className="form-field">
+            <fieldset className="form-inner">
                 <legend className="form-title">
                     Fill in your name and address details
                 </legend>
                 <div className="form-leadin">
                     <p className="form-leadin-text">
-                        Fehler mit einem
-                        <span className="form-required-symbol">
-                            * </span>
-                        sind Plichtfehlder
+                        Fehler mit einem <b className="form-notion">*</b> sind Plichtfehlder
                     </p>
-                    <p className="form-leading-errormessage">Fehlermeldung</p>
+                    <p
+                        className="form-leading-errormessage"
+                        id="error-message-name"
+                    >
+                        Fehlermeldung
+                    </p>
                 </div>
                 <div className="form-row">
-                    <label htmlFor="formName" className="form-label">
-                        Name
-                        <dfn className="form-required-symbol" title="Ist ein Pflichtfeld">
-                            *
-                        </dfn>
-                    </label>
-                    <div className="form-row-field">
-                        <input type="text" className="form-input" required="true" id="formName"/>
+                    <div className="b-formfield">
+                        <label
+                            htmlFor="formName"
+                            className="formfield-label"
+                        >
+                            Name
+                            <dfn
+                                className="form-notion"
+                                title="Ist ein Pflichtfeld"
+                            >
+                                *
+                            </dfn>
+                        </label>
+                        <div className="formfield-box is-error">
+                            <input
+                                type="text"
+                                className="form-input"
+                                required="true"
+                                aria-labelledby="error-message-name"
+                                id="formName"
+                            />
+                        </div>
                     </div>
                 </div>
                 <div className="form-row">
-                    <label htmlFor="formEmail" className="form-label">
-                        E-Mail
-                        <dfn className="form-required-symbol" title="Ist ein Pflichtfeld">
-                            *
-                        </dfn>
-                    </label>
-                    <div className="form-row-field">
-                        <input type="text" className="form-input" required="true" id="formEmail"/>
+                    <div className="b-formfield">
+                        <label
+                            htmlFor="formEmail"
+                            className="formfield-label"
+                        >
+                            E-Mail
+                            <dfn
+                                className="form-notion"
+                                title="Ist ein Pflichtfeld"
+                            >
+                                *
+                            </dfn>
+                        </label>
+                        <div className="formfield-box">
+                            <input
+                                type="email"
+                                className="form-input"
+                                required="true"
+                                id="formEmail"
+                            />
+                        </div>
                     </div>
                 </div>
                 <div className="form-row">
-                    <label htmlFor="formPhone" className="form-label">
-                        Telefon
-                    </label>
-                    <div className="form-row-field">
-                        <input type="text" className="form-input"id="forPhone"/>
+                    <div className="b-formfield">
+                        <label
+                            htmlFor="formPhone"
+                            className="formfield-label"
+                        >
+                            Telefon
+                        </label>
+                        <div className="formfield-box">
+                            <input
+                                type="number"
+                                className="form-input"
+                                id="forPhone"
+                            />
+                        </div>
                     </div>
                 </div>
                 <div className="form-row">
-                    <label htmlFor="formComments" className="form-label">
-                        Kommentar
-                        <dfn className="form-required-symbol" title="Ist ein Pflichtfeld">
-                            *
-                        </dfn>
-                    </label>
-                <div className="form-row-field">
-                    <textarea type="text" className="form-textarea" required="true" id="formComments" ></textarea>
-                </div>
+                    <div className="b-formfield">
+                        <label
+                            htmlFor="formComments"
+                            className="formfield-label"
+                        >
+                            Kommentar
+                            <dfn
+                                className="form-notion"
+                                title="Ist ein Pflichtfeld"
+                            >
+                                *
+                            </dfn>
+                        </label>
+                        <div className="formfield-box">
+                            <textarea
+                                className="form-textarea"
+                                required="true"
+                                maxLength="500"
+                                rows="5"
+                                id="formComments"
+                            >
+                            </textarea>
+                        </div>
+                    </div>
                 </div>
                 <div className="form-row">
                    <BtnDefault />
